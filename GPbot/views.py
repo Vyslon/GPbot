41efab0f9f2fe1196f51e0aca4cc41d01739e439
Vyslon <santoni.thomas26@hotmail.fr>
2018-10-03 16:17:27 +0200

    run.py fixed, I have tested interactions between config.py and other files of the project, useless variables are still on config.py and views.py

--- GPbot/views.py
+++ GPbot/views.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config.from_object('config')
+
 @app.route('/')
 def index():
-    return "test pour la route '/'"
+    return "var from config.py  :  " + app.config['TEST_VAR']
 
 if __name__ == "__main__":
     app.run()
