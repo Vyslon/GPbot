cd5eb25884f79655ae702df63334ca30fb2f67f5
Vyslon <santoni.thomas26@hotmail.fr>
2018-10-09 11:29:01 +0200

    Everything has been initialized to get informations about a location using MediaWIki API

--- GPbot/api_requests.py
+++ GPbot/api_requests.py
@@ -1,4 +1,6 @@
+# coding: utf-8
 import requests
+from unicodedata import normalize
 
 def get_lat_lng(adress, municipality, region):
     """
@@ -8,8 +10,19 @@ def get_lat_lng(adress, municipality, region):
     adress.replace(" ", "+")
     municipality.replace(" ", "+")
     region.replace(" ", "+")
-    URL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + adress + "," + municipality + "," + region + "&key=APIKEY"
+    URL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + adress + "," + municipality + "," + region + "&key=API_KEY"
     geocoding_maps_return = requests.get(URL).json()
     lat = geocoding_maps_return['results'][0]['geometry']['location']['lat']
     lng = geocoding_maps_return['results'][0]['geometry']['location']['lng']
     return (lat, lng)
+
+def get_location_info(location):
+    """
+        Get informations about a location using MediaWiki API
+        - Return a string 
+    """
+    location_infos = requests.get("https://fr.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" + location + "&utf8&indexpageids").json()
+    page_id = location_infos['query']['pageids'][0].encode('utf-8')
+    returned_info = location_infos['query']['pages'][page_id]['extract']
+
+    return returned_info.encode('utf-8')
--- test_api_requests.py
+++ test_api_requests.py
@@ -1,7 +1,12 @@
+#coding: utf-8
 import requests
-from GPbot.api_requests import get_lat_lng
+from GPbot.api_requests import get_lat_lng, get_location_info
+from unicodedata import normalize
 
 def test_api_return(monkeypatch):
-    lat_lng_result = (37.4231778, -122.0852514)
+    lat_lng_result = (37.4224629, -122.0855711)
+    location_desc = "Le Googleplex est le siège social de Google,"
 
     assert get_lat_lng("1600 amphitheatre parkway", "mountain view", "ca") == lat_lng_result
+
+    assert get_location_info("googleplex")[0:45] == location_desc
